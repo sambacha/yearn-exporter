da5c61103385c94e3d4b7eab619e626698d09a40
sam <sam@freighttrust.com>
2021-03-06 18:33:07 -0800

    refactor(dockerfile): adjustments
    reduce image size

--- Dockerfile
+++ Dockerfile
@@ -1,8 +1,15 @@
-FROM python:3.8-buster
+FROM python:3.8-buster-slim
+
+RUN mkdir -p /app/yearn-exporter
 
 RUN pip3 install eth-brownie prometheus_client dataclasses cachetools
-RUN mkdir -p /app/yearn-exporter
-ADD . /app/yearn-exporter
 WORKDIR /app/yearn-exporter
+
+
+COPY . /app/yearn-exporter
+COPY entrypoint.sh /usr/local/bin/
+
+EXPOSE 9091
+EXPOSE 9090
 
 ENTRYPOINT ["./entrypoint.sh"]
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,12 @@
-#! /bin/bash
+#!/bin/bash
 set -e
 
 NETWORK="mainnet" # default to Mainnet (Infura)
 EXPLORER="https://api.etherscan.io/api"
 
-if [[ ! -z "${ALCHEMY_URL}" ]]; then
-  if [[ ! $(brownie networks list | grep mainnet-alchemy) ]]; then
-    brownie networks add Ethereum mainnet-alchemy host=$ALCHEMY_URL chainid=1 explorer=$EXPLORER
+if [[ -n "${ALCHEMY_URL}" ]]; then
+  if find . | grep -q mainnet-alchemy; then
+    brownie networks add Ethereum mainnet-alchemy host="$ALCHEMY_URL" chainid=1 explorer=$EXPLORER
   fi
   NETWORK="mainnet-alchemy"
 fi
@@ -17,4 +17,4 @@ if [[ -z $1 ]]; then
 fi
 
 echo "Running brownie for $1 on network $NETWORK..."
-brownie run yearn $1 --network $NETWORK
+brownie run yearn "$1" --network $NETWORK
